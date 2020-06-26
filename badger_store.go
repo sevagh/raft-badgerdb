77b6c01cedfcf4329cd70f9e8816a9380c9c9ae4
Sevag Hanssian <sevagh@protonmail.com>
2020-06-26 17:19:13 -0400

    Commit work so far

---
+++ badger_store.go
@@ -0,0 +1,228 @@
+package raftbadgerdb
+
+import (
+	"errors"
+
+	badger "github.com/dgraph-io/badger/v2"
+	"github.com/hashicorp/raft"
+)
+
+var (
+	// Bucket names we perform transactions in
+	dbLogs = []byte("logs")
+	dbConf = []byte("conf")
+
+	// An error indicating a given key does not exist
+	ErrKeyNotFound = errors.New("not found")
+)
+
+// BadgerStore provides access to BadgerDB for Raft to store and retrieve
+// log entries. It also provides key/value storage, and can be used as
+// a LogStore and StableStore.
+type BadgerStore struct {
+	// conn is the underlying handle to the db.
+	conn *badger.DB
+
+	// The path to the Badger database file
+	path string
+}
+
+// NewBadgerStore takes a file path and returns a connected Raft backend.
+func NewBadgerStore(path string) (*BadgerStore, error) {
+	return New(badger.DefaultOptions(path))
+}
+
+// New uses the supplied options to open the BadgerDB and prepare it for use as a raft backend.
+func New(options badger.Options) (*BadgerStore, error) {
+	handle, err := badger.Open(options)
+	if err != nil {
+		return nil, err
+	}
+
+	store := &BadgerStore{
+		conn: handle,
+		path: options.Dir,
+	}
+
+	return store, nil
+}
+
+// Close is used to gracefully close the DB connection.
+func (b *BadgerStore) Close() error {
+	return b.conn.Close()
+}
+
+// FirstIndex returns the first known index from the Raft log.
+func (b *BadgerStore) FirstIndex() (uint64, error) {
+	tx, err := b.conn.NewTransaction(false)
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Discard()
+
+	iterator := tx.Get(dbLogs)
+	defer iterator.Close()
+
+	// check if the iterator is empty
+	if !iterator.Valid() {
+		return 0, nil
+	}
+
+	var ret uint64
+
+	// get the value thru an anonymous function without incurring
+	// copy cost
+	if firstItem := iterator.Item(); firstItem != nil {
+		firstItem.Value(func(val []byte) error {
+			ret = bytesToUint64(val)
+			return nil
+		})
+	}
+
+	if first, _ := curs.First(); first == nil {
+		return 0, nil
+	} else {
+		return bytesToUint64(first), nil
+	}
+}
+
+// LastIndex returns the last known index from the Raft log.
+func (b *BadgerStore) LastIndex() (uint64, error) {
+	tx, err := b.conn.Begin(false)
+	if err != nil {
+		return 0, err
+	}
+	defer tx.Rollback()
+
+	curs := tx.Bucket(dbLogs).Cursor()
+	if last, _ := curs.Last(); last == nil {
+		return 0, nil
+	} else {
+		return bytesToUint64(last), nil
+	}
+}
+
+// GetLog is used to retrieve a log from BadgerDB at a given index.
+func (b *BadgerStore) GetLog(idx uint64, log *raft.Log) error {
+	tx, err := b.conn.Begin(false)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	bucket := tx.Bucket(dbLogs)
+	val := bucket.Get(uint64ToBytes(idx))
+
+	if val == nil {
+		return raft.ErrLogNotFound
+	}
+	return decodeMsgPack(val, log)
+}
+
+// StoreLog is used to store a single raft log
+func (b *BadgerStore) StoreLog(log *raft.Log) error {
+	return b.StoreLogs([]*raft.Log{log})
+}
+
+// StoreLogs is used to store a set of raft logs
+func (b *BadgerStore) StoreLogs(logs []*raft.Log) error {
+	tx, err := b.conn.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	for _, log := range logs {
+		key := uint64ToBytes(log.Index)
+		val, err := encodeMsgPack(log)
+		if err != nil {
+			return err
+		}
+		bucket := tx.Bucket(dbLogs)
+		if err := bucket.Put(key, val.Bytes()); err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+// DeleteRange is used to delete logs within a given range inclusively.
+func (b *BadgerStore) DeleteRange(min, max uint64) error {
+	minKey := uint64ToBytes(min)
+
+	tx, err := b.conn.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	curs := tx.Bucket(dbLogs).Cursor()
+	for k, _ := curs.Seek(minKey); k != nil; k, _ = curs.Next() {
+		// Handle out-of-range log index
+		if bytesToUint64(k) > max {
+			break
+		}
+
+		// Delete in-range log index
+		if err := curs.Delete(); err != nil {
+			return err
+		}
+	}
+
+	return tx.Commit()
+}
+
+// Set is used to set a key/value set outside of the raft log
+func (b *BadgerStore) Set(k, v []byte) error {
+	tx, err := b.conn.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	bucket := tx.Bucket(dbConf)
+	if err := bucket.Put(k, v); err != nil {
+		return err
+	}
+
+	return tx.Commit()
+}
+
+// Get is used to retrieve a value from the k/v store by key
+func (b *BadgerStore) Get(k []byte) ([]byte, error) {
+	tx, err := b.conn.Begin(false)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	bucket := tx.Bucket(dbConf)
+	val := bucket.Get(k)
+
+	if val == nil {
+		return nil, ErrKeyNotFound
+	}
+	return append([]byte(nil), val...), nil
+}
+
+// SetUint64 is like Set, but handles uint64 values
+func (b *BadgerStore) SetUint64(key []byte, val uint64) error {
+	return b.Set(key, uint64ToBytes(val))
+}
+
+// GetUint64 is like Get, but handles uint64 values
+func (b *BadgerStore) GetUint64(key []byte) (uint64, error) {
+	val, err := b.Get(key)
+	if err != nil {
+		return 0, err
+	}
+	return bytesToUint64(val), nil
+}
+
+// Sync performs an fsync on the database file handle. This is not necessary
+// under normal operation unless NoSync is enabled, in which this forces the
+// database file to sync against the disk.
+func (b *BadgerStore) Sync() error {
+	return b.conn.Sync()
+}
---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/sevagh/raft-badgerdb
+
+go 1.14
+
+require (
+	github.com/dgraph-io/badger/v2 v2.0.3
+	github.com/hashicorp/go-msgpack v1.1.5
+	github.com/hashicorp/raft v1.1.2
+)
